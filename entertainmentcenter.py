782a06d15347d48b405b0088a8651206ce4287f1
mikeacre <mikeacre@gmail.com>
2016-12-26 19:55:28 -0500

    fixed
    fixed

--- entertainmentcenter.py
+++ entertainmentcenter.py
@@ -4,14 +4,16 @@ import fresh_tomatoes
 #create media objects of movies to display
 wolf_wall_st = media.Movie(
     "The Wolf of Wall st",
-    "This is the story of New York stockbroker, Jordan Belfort. From the American dream to corporate greed,\
-     Belfort goes from penny stocks and righteousness to IPOs and a life of corruption in the late 80s.",
+    "This is the story of New York stockbroker, Jordan Belfort. From the\
+     American dream to corporate greed, Belfort goes from penny stocks and\
+     righteousness to IPOs and a life of corruption in the late 80s.",
     "http://t1.gstatic.com/images?q=tbn:ANd9GcTm52BWbxXmwOpfz5rmx0BNBj79kSQoGpYPq4TVt07Jel5En6aC",
     "https://www.youtube.com/watch?v=iszwuX1AK6A", "50")
 
 forrest_gump = media.Movie(
     "Forrest Gump",
-    "Forrest Gump is a 1994 American comedy-drama film based on the 1986 novel of the same name by Winston Groom.",
+    "Forrest Gump is a 1994 American comedy-drama film based on the 1986\
+    novel of the same name by Winston Groom.",
     "http://t3.gstatic.com/images?q=tbn:ANd9GcQCFOcMb5_zkdZg4B4JvIGLlTQTvLdtLjiS5axJJDqP1FaI8Yyx",
     "https://www.youtube.com/watch?v=uPIEn0M8su0", "50")
 
@@ -23,5 +25,5 @@ d_wears_p = media.Movie(
 
 movies = [wolf_wall_st, forrest_gump, d_wears_p]
 
-#send objects and open display page. 
+#send objects and open display page.
 fresh_tomatoes.open_movies_page(movies)
